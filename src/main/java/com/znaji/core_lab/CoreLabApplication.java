32c37ee2a879b7574f120c90319b9691b154fc82
ZakaNaji <zakaria.naji@edu.uca.ma>
2025-09-30 10:18:52 +0100

    Init project

---
+++ src/main/java/com/znaji/core_lab/CoreLabApplication.java
@@ -0,0 +1,13 @@
+package com.znaji.core_lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/znaji/core_lab/CoreLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.znaji.core_lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
